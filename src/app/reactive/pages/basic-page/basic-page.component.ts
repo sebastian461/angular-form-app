cb357d7d2a08ea3dd081758746ed82d1a4a530d2
sebastian461 <sebastiano-461@hotmail.com>
2024-03-21 14:46:45 -0500

    Creación del formulario reactivo

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
   templateUrl: './basic-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class BasicPageComponent {
+  //* Este es un formulario con FormControl
+  /* public myForm: FormGroup = new FormGroup({
+    name: new FormControl('', [], []), //valor inicial - validaciónes sincronas - validaciones ásincronas
+    price: new FormControl(0),
+    inStorage: new FormControl(0),
+  }); */
 
+  //* Este es un formulario con FormBuilder
+  public myForm: FormGroup = this.fb.group({
+    name: [''],
+    price: [0],
+    inStorage: [0],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSave(): void {
+    console.log(this.myForm.value);
+  }
 }
