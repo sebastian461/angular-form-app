a80f37e5c570d8183c4487fcaff43f130e7f4b77
sebastian461 <sebastiano-461@hotmail.com>
2024-03-21 14:07:31 -0500

    Estructura HTML de los componentes

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-page',
+  templateUrl: './basic-page.component.html',
+  styles: ``
+})
+export class BasicPageComponent {
+
+}
---
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-page',
+  templateUrl: './dynamic-page.component.html',
+  styles: ``
+})
+export class DynamicPageComponent {
+
+}
