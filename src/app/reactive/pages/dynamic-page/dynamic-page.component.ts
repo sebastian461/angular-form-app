8afeab40473d6dae49cbaf6271c8e833dcd86ad7
sebastian461 <sebastiano-461@hotmail.com>
2024-03-22 14:56:29 -0500

    Formulario dinamico: validaciones

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -21,6 +21,39 @@ export class DynamicPageComponent {
     return this.myForm.get('favoritGames') as FormArray;
   }
 
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  isValidFieldInArray(formArray: FormArray, index: number): boolean | null {
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Minimo ${errors['minlength'].requiredLength} caracteres`;
+      }
+    }
+
+    return null;
+  }
+
+  onDeleteFavorite(index: number): void {
+    this.getFavoritGames.removeAt(index);
+  }
+
   onSumit(): void {
     if (this.myForm.invalid) {
       this.myForm.markAllAsTouched();
