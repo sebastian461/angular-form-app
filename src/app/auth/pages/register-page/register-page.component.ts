9dc680beec67bab92621e7f46c34262b74842355
sebastian461 <sebastiano-461@hotmail.com>
2024-04-04 16:06:26 -0500

    Centralización de validaciones

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+/* import {
+  cantBeStrider,
+  emailPattern,
+  firstNameAndLastnamePattern,
+} from '../../../shared/validators/validators'; */
+
+import { ValidatorsService } from '../../../shared/services/validators.service';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class RegisterPageComponent {
+  public myForm = this.fb.group({
+    name: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(this.validatorsService.firstNameAndLastnamePattern),
+      ],
+    ],
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(this.validatorsService.emailPattern),
+      ],
+    ],
+    username: ['', [Validators.required, this.validatorsService.cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
 
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService
+  ) {}
+
+  isValidField(field: string): boolean | null {
+    return this.validatorsService.isValidField(this.myForm, field);
+  }
+
+  onSubmit(): void {
+    this.myForm.markAllAsTouched();
+  }
 }
